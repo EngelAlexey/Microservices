dd71bdf767af0452b20d5d7f7a33c985ed1efaa8
EngelAlexey <alexhmanzanares@gmail.com>
2026-02-11 13:56:19 -0600

    Fix import

--- inventory-microservice/models.py
+++ inventory-microservice/models.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, String, Float, Date, DateTime, Text, Integer, DECIMAL
+from sqlalchemy import Column, String, Float, Date, DateTime, Text, Integer, DECIMAL, Boolean
 from database import Base
 import datetime
 
 class BcItem(Base):
     __tablename__ = "bcItems"
     ItemID = Column(String(20), primary_key=True)
+    DatabaseID = Column(String(10))
     itCode = Column(String(50))
     CabysID = Column(String(20))
     itTitle = Column(String(300))
@@ -58,3 +59,39 @@ class FnDocumentLn(Base):
     dlTotal = Column(DECIMAL(13, 2))
     
     dlObservations = Column(String(2000), nullable=True)
+
+class IcMovement(Base):
+    __tablename__ = "icMovements"
+    
+    MovementID = Column(String(10), primary_key=True)
+    isDeleted = Column(Boolean, default=False)
+    DatabaseID = Column(String(10))
+    OriginID = Column(String(10))     
+    ProjectID = Column(String(10))    
+    ItemID = Column(String(10))       
+    DocumentLnID = Column(String(10)) 
+    mvDate = Column(DateTime, default=datetime.datetime.now)
+    mvAction = Column(String(10))     
+    mvQuantity = Column(DECIMAL(13, 2))
+    mvStatus = Column(String(45), default="Applied")
+    mvNotes = Column(Text)
+    mvCreatedby = Column(String(10), default="AI_BOT")
+    mvCreateddate = Column(DateTime, default=datetime.datetime.now)
+
+class IcPrice(Base):
+    __tablename__ = "icPrices"
+    
+    PriceID = Column(String(10), primary_key=True)
+    isDeleted = Column(Boolean, default=False)
+    DatabaseID = Column(String(10))
+    ItemID = Column(String(10))
+    ProjectID = Column(String(10))
+    MovementID = Column(String(10)) 
+    prTitle = Column(String(150))   
+    prDescription = Column(Text)
+    prQuantity = Column(DECIMAL(13, 2))
+    prPrice = Column(DECIMAL(13, 2)) 
+    prTax = Column(DECIMAL(13, 2), default=0)
+    prTotal = Column(DECIMAL(13, 2))
+    prCreatedby = Column(String(10), default="AI_BOT")
+    prCreateddate = Column(DateTime, default=datetime.datetime.now)
